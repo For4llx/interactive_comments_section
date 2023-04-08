cf245a110254f2d677b2c38855a9990d94a6200c
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-04-08 11:18:47 +0200

    added comments serializer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "authentification",
     "comments",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- backend/comments/models.py
+++ backend/comments/models.py
@@ -3,11 +3,11 @@ from authentification.models import User
 
 
 class Comment(models.Model):
-    content = models.CharField(max_length=255)
+    content = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    replies = models.ManyToManyField("self", symmetrical=False)
+    replies = models.ManyToManyField("self", symmetrical=False, blank=True)
 
     def __str__(self):
         return self.user.username
