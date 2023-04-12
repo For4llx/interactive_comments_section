dd562bbd32950c8531eb16d65b069e43ade2a1ac
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-04-12 17:07:30 +0200

    modified

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -6,10 +6,10 @@ from authentification.models import User
 class Comment(models.Model):
     content = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
-    score = models.IntegerField()
+    score = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reply = models.BooleanField(blank=True, null=True)
-    replies = models.ManyToManyField("self", symmetrical=False, blank=True)
+    replies = models.ManyToManyField("self", symmetrical=False, default=[])
 
     def __str__(self):
         return self.user.username
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from .models import Comment
+from authentification.models import User
 from .serializers import CommentSerializer
 
 
 class CommentViewset(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def create(self, request):
+        user = User.objects.get(username=request.data["user.username"])
+        comment_data = {
+            "user": user,
+            "content": request.data["content"],
+            "score": request.data["score"],
+            "reply": False,
+        }
+        new_comment = Comment(**comment_data)
+        new_comment.save()
+        return Response({"status": "Custom action completed"})
